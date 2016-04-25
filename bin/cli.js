d00900988db12a1db51b6408ed7ef5912edb98af
Scott Public <scottpublic@Scotts-MBP.home>
2016-04-24 21:51:36 -0400

    Added command line parsing

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
+'use strict';
+
+var _minimist = require('minimist');
+
+var _minimist2 = _interopRequireDefault(_minimist);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var argv = (0, _minimist2.default)(process.argv.slice(2));
+
+console.log(JSON.stringify(argv._));
 console.log('Done');
--- cli.es6
+++ cli.es6
@@ -1,1 +1,6 @@
+import minimist from 'minimist';
+
+var argv = minimist(process.argv.slice(2));
+
+console.log(JSON.stringify(argv._));
 console.log('Done');
